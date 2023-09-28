45592ff5a544c71f087dc5a6294ecd9a5305e030
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:35 +0400

    add Warrior.cs

---
+++ ConsoleApp9/Warrior.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp9;
+
+public class Warrior : Character
+{
+    public Warrior(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+        : base(name, health, damage, armor, strength, agility, intellect)
+    {
+    }
+
+    public override void Ability()
+    {
+        BattleCry();
+    }
+
+    private void BattleCry()
+    {
+        Damage *= 1.5;
+    }
+}
