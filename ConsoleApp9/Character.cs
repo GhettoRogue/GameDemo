5933f100ecce6fd1c4e17cc1a6344e3a3b589169
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:18:58 +0400

    add Character.cs

---
+++ ConsoleApp9/Character.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp9;
+
+public abstract class Character : IStats
+{
+    public string Name { get; set; }
+    public double Health { get; set; }
+    public double Damage { get; set; }
+    public double Armor { get; set; }
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Intellect { get; set; }
+
+
+    protected Character(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+    {
+        Name = name;
+        Health = health;
+        Damage = damage;
+        Armor = armor;
+        Strength = strength;
+        Agility = agility;
+        Intellect = intellect;
+    }
+
+    public abstract void Ability();
+}
