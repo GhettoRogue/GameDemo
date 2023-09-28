26d9c67683cdd2b3b33f546d6e64850c0108c252
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:09 +0400

    add Rogue.cs

---
+++ ConsoleApp9/Rogue.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp9;
+
+public class Rogue : Character
+{
+    public Rogue(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+        : base(name, health, damage, armor, strength, agility, intellect)
+    {
+    }
+
+
+    public override void Ability()
+    {
+        ShadowStrike();
+    }
+
+    private void ShadowStrike()
+    {
+        Damage *= 1.5;
+    }
+}
