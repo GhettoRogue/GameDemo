c4307fedb92b89e16770a8e67ee75eccfb92c78d
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:52 +0400

    add Stats.cs

---
+++ ConsoleApp9/Stats.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp9;
+
+public interface IStats
+{
+    public double Health { get; set; }
+    public double Damage { get; set; }
+    public double Armor { get; set; }
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Intellect { get; set; }
+}
