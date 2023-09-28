85f37320ea11ac796cf99fa4e15d05ec2238d1ee
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:18 +0400

    add Mage.cs

---
+++ ConsoleApp9/Mage.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp9;
+
+public class Mage : Character
+{
+    public Mage(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+        : base(name, health, damage, armor, strength, agility, intellect)
+    {
+    }
+
+    public override void Ability()
+    {
+        MagicShield();
+    }
+
+    private void MagicShield()
+    {
+        Armor *= 2.5;
+    }
+}
