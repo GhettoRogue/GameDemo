548c2959f1ba3f36cf8fda38a34bf4e0b851260c
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:43 +0400

    add Druid.cs

---
+++ ConsoleApp9/Druid.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp9;
+
+public class Druid : Character
+{
+    public Druid(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+        : base(name, health, damage, armor, strength, agility, intellect)
+    {
+    }
+
+    public override void Ability()
+    {
+        BearForm();
+    }
+
+    private void BearForm()
+    {
+        Health *= 2.5;
+    }
+}
