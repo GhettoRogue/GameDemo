c001ccdb269f0af0894929ae7a54a73ac96ed12c
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-28 14:19:27 +0400

    add Priest.cs

---
+++ ConsoleApp9/Priest.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp9;
+
+public class Priest : Character
+{
+    public Priest(string name, int health, int damage, int armor, int strength, int agility, int intellect)
+        : base(name, health, damage, armor, strength, agility, intellect)
+    {
+    }
+
+    public override void Ability()
+    {
+        LastStand();
+    }
+
+    private void LastStand()
+    {
+        Health *= 1.5;
+    }
+}
